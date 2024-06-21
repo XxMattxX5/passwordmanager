dbff218300c1cbe5d86039a86831de3196835ca6
XxMattxX5 <116413060+XxMattxX5@users.noreply.github.com>
2024-06-21 17:09:12 -0400

    Removed need for user timezone

--- backend/serializers.py
+++ backend/serializers.py
@@ -2,20 +2,20 @@ from datetime import datetime
 import utils
 
 # Serializer for passwords
-def serialize_password(password, timezone):
+def serialize_password(password):
     return {
         "id": password.id,
         "name": password.account_name,
         "username": password.username,
         "password": password.password,
-        "created": utils.convert_to_timezone(password.created_at,timezone).strftime('%Y-%m-%d %H:%M:%S')
+        "created": password.created_at.strftime('%Y-%m-%d %H:%M:%S')
     }
 
 # Serializer for folders
-def serialize_folder(folder, timezone):
+def serialize_folder(folder):
     return {
         "id": folder.id,
         "name": folder.name,
-        "created": utils.convert_to_timezone(folder.created_at, timezone).strftime('%Y-%m-%d %H:%M:%S'),
-        "accounts": [serialize_password(password, timezone) for password in folder.passwords]
+        "created": folder.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+        "accounts": [serialize_password(password) for password in folder.passwords]
     }
